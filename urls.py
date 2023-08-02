2849a134c9ea9a8e528baf3a150e4592b726824a
Dave Syer <dsyer@vmware.com>
2023-08-02 11:22:02 +0100

    Add static content

--- urls.py
+++ urls.py
@@ -3,4 +3,5 @@ from views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('stream', stream, name='stream'),
 ]
--- views.py
+++ views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+import datetime
+import time
+from django.http import StreamingHttpResponse
+
+def stream(request):
+    def event_stream():
+        while True:
+            time.sleep(3)
+            yield 'data: The server time is: %s\n\n' % datetime.datetime.now()
+    return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
 
 def home(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
