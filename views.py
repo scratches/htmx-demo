f84d17fefb6bc7ca65481fc33f749ae77113b85e
Dave Syer <dsyer@vmware.com>
2023-08-02 11:43:46 +0100

    Emit HTML

--- views.py
+++ views.py
@@ -6,9 +6,12 @@ from django.http import StreamingHttpResponse
 
 def stream(request):
     def event_stream():
+        template = loader.get_template('time.html')
+        value = 0
         while True:
             time.sleep(3)
-            yield 'data: The server time is: %s\n\n' % datetime.datetime.now()
+            value = value + 1
+            yield 'data:<div>Index: %d, Time: %s</div>\n\n' % (value, datetime.datetime.now().isoformat())
     return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
 
 def home(request):
